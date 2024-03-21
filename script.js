4a9e97c16203743a62a042c1f961e0cbbf08e24b
Dexter8749 <argyelan.mark@szbiszeged.hu>
2024-03-21 10:31:48 +0100

    sad

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
 class adat{
-construct(nev, leiras){
-    this.nev = nev;
-    this.leiras = leiras;
-    }
-}
-
-document.getElementById('kuld').addEventListener('click', function(){
-    
-    xhttp = new XMLHttpRequest(); //oBJEKTUMOT Hoz létre. Példányosítás
-    
-    xhttp.onload = function(){ //névtelen function
-        
-        let keres =  document.getElementById("keres").value;
-        let lista = keres.split(";");
-        let telefonok = [];
-        for (let i=0;i<lista.length; i++){
-            let splitted = list[i].split(':');
-            telefonok.push(new adat(splitted[0],splitted[1]));
+ construct(nev, leiras){
+        this.nev = nev;
+        this.leiras = leiras;
         }
+    }
+    
+    document.getElementById('kuld').addEventListener('click', function(){
         
+        xhttp = new XMLHttpRequest(); //oBJEKTUMOT Hoz létre. Példányosítás
         
+        xhttp.onload = function(){ //névtelen function
+            
+            let keres =  document.getElementById("keres1").value;
+            let szoveg = xhttp.responseText; 
+            let lista = szoveg.split(";");
+    
+            let telefonok = [];
+            for (let i=0;i<lista.length; i++){
+                let splitted = lista[i].split(':');
+                telefonok.push(new adat(splitted[0],splitted[1]));
+            }
+            let talalat = szures(telefonok, keres);
+            console.log(talalat);
+            
+        }
+        xhttp.open("POST", "adat/forras.txt");
+        xhttp.send();
     });            
-    
-    
+        
+        
     function szures(telefonok, keresett) {
     
         for (let i = 0; i < telefonok.length; i++) {
             if (telefonok[i].nev === keresett) {
-              return telefonok[i].leiras;
+                return telefonok[i].leiras;
             }
-          }
-          return "Nem található ilyen névvel rendelkező telefon.";
+            }
+            return "Nem található ilyen névvel rendelkező telefon.";
         }
+    
+    
